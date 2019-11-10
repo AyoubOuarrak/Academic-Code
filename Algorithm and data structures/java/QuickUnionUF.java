ffd52599da7acd18ce370d0e7381387062bd1b57
Ayoub Ouarrak <ouarrakayoub@gmail.com>
2019-11-10 17:59:00 +0100

    Added java algorithms

---
+++ Algorithm and data structures/java/QuickUnionUF.java
@@ -0,0 +1,79 @@
+QuickUnionUF.java
+
+
+Below is the syntax highlighted version of QuickUnionUF.java from §1.5 Case Study: Union-Find.   Here is the Javadoc.
+
+
+/****************************************************************************
+ *  Compilation:  javac QuickUnionUF.java
+ *  Execution:  java QuickUnionUF < input.txt
+ *  Dependencies: StdIn.java StdOut.java
+ *
+ *  Quick-union algorithm.
+ *
+ ****************************************************************************/
+
+/**
+ *  The <tt>QuickUnionUF</tt> class represents a union-find data structure.
+ *  It supports the <em>union</em> and <em>find</em> operations, along with
+ *  methods for determinig whether two objects are in the same component
+ *  and the total number of components.
+ *  <p>
+ *  This implementation uses quick union.
+ *  Initializing a data structure with <em>N</em> objects takes linear time.
+ *  Afterwards, <em>union</em>, <em>find</em>, and <em>connected</em> take
+ *  time linear time (in the worst case) and <em>count</em> takes constant
+ *  time.
+ *  <p>
+ *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/15uf">Section 1.5</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ */
+public class QuickUnionUF {
+    private int[] id;    // id[i] = parent of i
+    private int count;   // number of components
+
+    /**
+     * Initializes an empty union-find data structure with N isolated components 0 through N-1.
+     * @throws java.lang.IllegalArgumentException if N < 0
+     * @param N the number of objects
+     */
+    public QuickUnionUF(int N) {
+        id = new int[N];
+        count = N;
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    /**
+     * Returns the number of components.
+     * @return the number of components (between 1 and N)
+     */
+    public int count() {
+        return count;
+    }
+
+    /**
+     * Returns the component identifier for the component containing site <tt>p</tt>.
+     * @param p the integer representing one site
+     * @return the component identifier for the component containing site <tt>p</tt>
+     * @throws java.lang.IndexOutOfBoundsException unless 0 <= p < N
+     */
+    public int find(int p) {
+        while (p != id[p])
+            p = id[p];
+        return p;
+    }
+
+    /**
+     * Are the two sites <tt>p</tt> and <tt>q</tt> in the same component?
+     * @param p the integer representing one site
+     * @param q the integer representing the other site
+     * @return <tt>true</tt> if the sites <tt>p</tt> and <tt>q</tt> are in the same
+     *    component, and <tt>false</tt> otherwise
+     * @throws java.lang.IndexOutOfBoundsException unless both 0 <= p < N and 0 <= q < N
+     */
+    public boolean connected(int p,
